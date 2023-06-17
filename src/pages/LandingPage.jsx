755f0007037f7e45e32ab8acdaca7a9050895ec6
FrontEndDanni <dannikabdull@gmail.com>
2023-06-17 01:43:18 -0700

    set up landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styling/landingpage.css'
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to RendezView</h1>
+      <p>Coordinate your plans effortlessly</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default LandingPage;
