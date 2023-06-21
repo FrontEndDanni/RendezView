fc51cea74647d003f9571fe889f1b2800f6e10af
FrontEndDanni <dannikabdull@gmail.com>
2023-06-20 23:14:26 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -17,4 +19,3 @@ function App() {
 }
 
 export default App;
-
